07d6399256002ba8ce499ca702dd1c25e27d3ac0
Louis Collard <louiscollard@chromium.org>
2018-01-10 18:16:59 +0800

    [alsa_helpers] Add option to print list of card names.
    BUG=chromium:796062
    TEST=Ran on eve.
    
    Change-Id: If72e215ad2c3f9f8df7eafef17616de8cc59c931
    Reviewed-on: https://chromium-review.googlesource.com/861314
    Commit-Ready: Louis Collard <louiscollard@chromium.org>
    Tested-by: Louis Collard <louiscollard@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>

--- src/alsa_helpers.c
+++ src/alsa_helpers.c
@@ -62,14 +62,49 @@ static void print_supported_capture_channels(const char *dev)
     snd_pcm_close(handle);
 }
 
+static void print_card_names() {
+    int card = -1;
+    snd_ctl_t *handle;
+    snd_ctl_card_info_t *info;
+    snd_ctl_card_info_alloca(&info);
+
+    if (snd_card_next(&card) < 0 || card < 0) {
+        fprintf(stderr, "Failed to get devices.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (card >= 0) {
+        char name[16];
+        sprintf(name, "hw:%d", card);
+
+        if (snd_ctl_open(&handle, name, 0) < 0) {
+            fprintf(stderr, "Failed to open device: %s\n", name);
+            exit(EXIT_FAILURE);
+        }
+
+        if (snd_ctl_card_info(handle, info) < 0) {
+            fprintf(stderr, "Failed to get info for device: %s\n", name);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("%d,%s\n", card, snd_ctl_card_info_get_name(info));
+
+        snd_ctl_close(handle);
+
+        if (snd_card_next(&card) < 0) {
+            fprintf(stderr, "Failed to get next card\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
 /* This program has helper functions that are used by autotests. The returned
    values are printed in stdout. Debug and error logs are printed in stderr.
    If the program fails to get the requested value, it returns 1. */
 int main(int argc, char *argv[])
 {
-    int c, get_capture_channels = 0;
-    const char *short_opt = "hd:c";
+    int c, get_capture_channels = 0, list_card_names = 0;
+    const char *short_opt = "hd:cl";
     char *dev = "hw:0,0";
 
     struct option long_opt[] =
@@ -77,6 +112,7 @@ int main(int argc, char *argv[])
        {"help",                  no_argument,       NULL, 'h'},
        {"device",                required_argument, NULL, 'd'},
        {"get_capture_channels",  no_argument,       NULL, 'c'},
+       {"list_card_names",       no_argument,       NULL, 'l'},
        {NULL,                    0,                 NULL, 0  }
     };
 
@@ -94,14 +130,19 @@ int main(int argc, char *argv[])
            get_capture_channels = 1;
            break;
 
+       case 'l':
+           list_card_names = 1;
+           break;
+
        case 'h':
            printf("Usage: %s [OPTIONS]\n", argv[0]);
-           printf("  -d, --device <Device>      Device, default to hw:0,0\n");
-           printf("  -h, --help                 Print this help and exit\n");
-           printf("  -c --get_capture_channels  Get supported channels of the "
-                                               "capture device\n");
+           printf("  -d, --device <Device>       Device, default to hw:0,0\n");
+           printf("  -h, --help                  Print this help and exit\n");
+           printf("  -c, --get_capture_channels  Get supported channels of the "
+                                                "capture device\n");
+           printf("  -l, --list_card_names       List all cards including "
+                                                "names\n");
            return(EXIT_SUCCESS);
-           break;
 
        default:
            fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
@@ -111,6 +152,11 @@ int main(int argc, char *argv[])
 
     if (get_capture_channels) {
         print_supported_capture_channels(dev);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (list_card_names) {
+        print_card_names();
         exit(EXIT_SUCCESS);
     }
 
