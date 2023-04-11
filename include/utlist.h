8cbea41697ff54cca4f91a2cad801480e5706a79
whalechang <whalechang@google.com>
2023-04-11 12:02:31 +0800

    Fix check_cros_license for presubmit
    BUG=None
    TEST=repo upload .
    
    Change-Id: I4e61cdac435d72c5ed369e39c3b246acb351b887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/audiotest/+/4408861
    Tested-by: Ching Yun Chang <whalechang@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>

--- include/utlist.h
+++ include/utlist.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2007-2011, Troy D. Hanson   http://uthash.sourceforge.net
+Copyright 2007-2011, Troy D. Hanson   http://uthash.sourceforge.net
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
