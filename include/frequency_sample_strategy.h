fdfdd500da42a68a49bb5dc7f16bee870fefad69
Cheng Yueh <cyueh@chromium.org>
2023-04-13 14:20:39 +0800

    audiofuntest: Allow serial and random frequency_sample_strategy
    There are many strategies for allowing factories to run this test in
    parallel. It's a trade-off between time and false positive rate.
    
    Allow serial and random frequency sample strategy in this CL.
    
    Contributers can add more strategies by extending FrequencyGenerator.
    
    BUG=b:276682478
    TEST=(1) cros-workon-brya start audiotest
         (2) emerge-brya audiotest
         (3) cros deploy ${DUT_IP} audiotest --root /usr/local
         (4) play_command=(sox -b16 -c2 -esigned -r48000 \
                           -traw - -b16 -esigned -talsa hw:0,0)
         (5) record_command=(sox -talsa hw:0,99 -b16 -c2 -esigned \
                             -r48000 -traw - remix 1 2 gain 20.0)
         (6) (DUT) audiofuntest -P "${play_command[*]}" \
                                -R "${record_command[*]}" \
                                -t s16 -I 48000 -O 48000 -T 10 -a 0 \
    			    -c 2 -C 2 -g 30 -i 4000 \
                                -x 10000 -p 0.045000 \
    			    -s random
         (7) (DUT) audiofuntest -P "${play_command[*]}" \
                                -R "${record_command[*]}" \
                                -t s16 -I 48000 -O 48000 -T 10 -a 0 \
                                -c 2 -C 2 -g 30 -i 4000 \
                                -x 10000 -p 0.045000 \
                                -s serial
    
    Change-Id: Ic7c00e1a54897fcb7a92bbbacb8c4fffe7ad1de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/audiotest/+/4421952
    Tested-by: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

---
+++ include/frequency_sample_strategy.h
@@ -0,0 +1,71 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef INCLUDE_FREQUENCY_SAMPLE_STRATEGY_H_
+#define INCLUDE_FREQUENCY_SAMPLE_STRATEGY_H_
+
+#include <memory>
+#include <random>
+#include <string_view>
+
+enum class FrequencySampleStrategy {
+  kSerial,
+  kRandom,
+  kUnknown,
+};
+
+// Convert FrequencySampleStrategy to string_view.
+std::string_view to_string_view(FrequencySampleStrategy s);
+
+// Create FrequencySampleStrategy from string_view.
+FrequencySampleStrategy from_string_view(std::string_view);
+
+class FrequencyGenerator {
+ public:
+  virtual ~FrequencyGenerator() {}
+  virtual int GetBin(int round) = 0;
+};
+
+class SerialFrequencyGenerator : public FrequencyGenerator {
+ public:
+  SerialFrequencyGenerator(int min_frequency,
+                           int max_frequency,
+                           int test_rounds,
+                           double frequency_resolution)
+      : min_bin(min_frequency / frequency_resolution),
+        max_bin(max_frequency / frequency_resolution),
+        bin_interval((max_bin - min_bin) / (test_rounds - 1)) {}
+
+  int GetBin(int round) override {
+    return min_bin + (round - 1) * bin_interval;
+  }
+
+ private:
+  int min_bin;
+  int max_bin;
+  int bin_interval;
+};
+
+class RandomFrequencyGenerator : public FrequencyGenerator {
+ public:
+  RandomFrequencyGenerator(int min_frequency,
+                           int max_frequency,
+                           int test_rounds,
+                           double frequency_resolution);
+
+  int GetBin(int round) override;
+
+ private:
+  std::mt19937 rng;
+  std::uniform_int_distribution<int> bin_distribution;
+};
+
+std::unique_ptr<FrequencyGenerator> make_frequency_generator(
+    FrequencySampleStrategy s,
+    int min_frequency,
+    int max_frequency,
+    int test_rounds,
+    double frequency_resolution);
+
+#endif  // INCLUDE_FREQUENCY_SAMPLE_STRATEGY_H_
