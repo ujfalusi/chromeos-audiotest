a73845cc55f54d30abfeddfda9a0d364e0f7c51d
Cheng Yueh <cyueh@chromium.org>
2023-04-13 14:20:39 +0800

    audiofuntest: Allow serial and random frequency_sample_strategy
    There are many strategies for allowing factories to run this test in
    parallel. It's a trade-off between time and false positive rate.
    
    Allow serial and random frequency sample strategy in this CL.
    
    Contributers can add more strategies by extending FrequencyGenerator.
    
    BUG=b:276682478
    TEST=(1) cros-workon-brya start audiotest
         (2) emerge-brya audiotest
         (3) cros deploy ${DUT_IP} audiotest --root /usr/local
         (4) play_command=(sox -b16 -c2 -esigned -r48000 \
                           -traw - -b16 -esigned -talsa hw:0,0)
         (5) record_command=(sox -talsa hw:0,99 -b16 -c2 -esigned \
                             -r48000 -traw - remix 1 2 gain 20.0)
         (6) (DUT) audiofuntest -P "${play_command[*]}" \
                                -R "${record_command[*]}" \
                                -t s16 -I 48000 -O 48000 -T 10 -a 0 \
    			    -c 2 -C 2 -g 30 -i 4000 \
                                -x 10000 -p 0.045000 \
    			    -s random
         (7) (DUT) audiofuntest -P "${play_command[*]}" \
                                -R "${record_command[*]}" \
                                -t s16 -I 48000 -O 48000 -T 10 -a 0 \
                                -c 2 -C 2 -g 30 -i 4000 \
                                -x 10000 -p 0.045000 \
                                -s serial
    
    Change-Id: Ic7c00e1a54897fcb7a92bbbacb8c4fffe7ad1de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/audiotest/+/4421952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/audiotest/+/4525700
    Auto-Submit: Phoebe Wang <phoebewang@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Phoebe Wang <phoebewang@chromium.org>

---
+++ src/frequency_sample_strategy.cc
@@ -0,0 +1,75 @@
+// Copyright 2023 The ChromiumOS Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <algorithm>
+#include <array>
+
+#include "include/frequency_sample_strategy.h"
+
+namespace {
+constexpr std::array<std::string_view,
+                     static_cast<std::size_t>(
+                         FrequencySampleStrategy::kUnknown)>
+    kFrequencySampleStrategyStrings{
+        "serial",
+        "random",
+    };
+void SeedMt19937(std::mt19937& rng) {
+  /* TODO(cyueh): Log seed for reproducing errors. */
+  std::array<std::mt19937::result_type, std::mt19937::state_size> state{};
+  std::generate(state.begin(), state.end(), std::random_device{});
+  std::seed_seq s(state.begin(), state.end());
+  rng.seed(s);
+}
+}  // namespace
+
+std::string_view to_string_view(FrequencySampleStrategy s) {
+  auto index = static_cast<std::size_t>(s);
+  if (index < kFrequencySampleStrategyStrings.size()) {
+    return kFrequencySampleStrategyStrings.at(index);
+  }
+  return "unknown";
+}
+
+FrequencySampleStrategy from_string_view(std::string_view sv) {
+  for (std::size_t index = 0; index < kFrequencySampleStrategyStrings.size();
+       ++index) {
+    auto& s = kFrequencySampleStrategyStrings.at(index);
+    if (s == sv) {
+      return static_cast<FrequencySampleStrategy>(index);
+    }
+  }
+  return FrequencySampleStrategy::kUnknown;
+}
+
+RandomFrequencyGenerator::RandomFrequencyGenerator(int min_frequency,
+                                                   int max_frequency,
+                                                   int test_rounds,
+                                                   double frequency_resolution)
+    : rng{},
+      bin_distribution{static_cast<int>(min_frequency / frequency_resolution),
+                       static_cast<int>(max_frequency / frequency_resolution)} {
+  SeedMt19937(rng);
+}
+
+int RandomFrequencyGenerator::GetBin(int round) {
+  return bin_distribution(rng);
+}
+
+std::unique_ptr<FrequencyGenerator> make_frequency_generator(
+    FrequencySampleStrategy s,
+    int min_frequency,
+    int max_frequency,
+    int test_rounds,
+    double frequency_resolution) {
+  switch (s) {
+    case FrequencySampleStrategy::kRandom:
+      return std::make_unique<RandomFrequencyGenerator>(
+          min_frequency, max_frequency, test_rounds, frequency_resolution);
+    default:
+    case FrequencySampleStrategy::kSerial:
+      return std::make_unique<SerialFrequencyGenerator>(
+          min_frequency, max_frequency, test_rounds, frequency_resolution);
+  }
+}
